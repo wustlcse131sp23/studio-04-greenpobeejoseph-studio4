f671244154630222c54bd2ebb502e326ad4d3a17
jj <jj@localhost>
2023-02-23 14:00:51 -0600

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,24 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(.5, .5, .4, .2);
+		
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(.5, .5, .4, .2);
+
+		StdDraw.setPenColor(Color.red);
+
+		StdDraw.filledRectangle(.5, .5, .15, .2);
+		
+		StdDraw.setPenColor(Color.white);
+		StdDraw.filledCircle(0.5, 0.5, 0.05);
+		
+
+		
+		
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,16 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		
+		Color black = new Color(0, 0, 0);
+		StdDraw.setPenColor(Color.red);
+		StdDraw.filledRectangle(0.5, 0.5, 0.2, 0.2);
+
+		
+		
+		
+		
 		
 	}
 }
